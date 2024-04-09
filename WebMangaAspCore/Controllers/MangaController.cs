1a8a1a3714f091547f53800098f524c3881c7a94
NabilDesTarterets <jalil.haddar@lmdsio.fr>
2024-04-09 12:32:49 +0100

    ajout index manga

--- WebMangaAspCore/Controllers/MangaController.cs
+++ WebMangaAspCore/Controllers/MangaController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using WebmangaAspCore.Models.MesExceptions;
 using WebmangaAspCore.Models.Metier;
 using WebmangaAspCore.Models.Dao;
